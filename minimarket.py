c07bf4a57c7fa1509585d0cec9091a13c917133f
Elaysha <celayshasantiagomorel@gmail.com>
2025-02-26 19:13:59 -0400

    Estructura del menu principal

--- minimarket.py
+++ minimarket.py
@@ -1,1 +1,30 @@
+import os
 lista_productos =[]
+
+while True:
+    
+    os.system("cls")
+    print("Esperanza minimarket")
+    print("1. Productos")
+    print("2. Clientes")
+    print("3. Facturar")
+    print("4. Datos del minimarket")
+    print("5. Salir")
+    
+    print("Seleccione una opcion")
+    op = int(input("Opcion: "))
+    
+    match op: 
+        case 1:
+            print('Productos')
+        case 2:
+            print('Clientes')
+        case 3:
+           print('Facturar')
+        case 4:
+            print("Datos del minimarket")
+            input("Presione para continuar")
+        case 5:
+            print("Salir")
+            print("Gracias por usar el minimarket")
+            break
